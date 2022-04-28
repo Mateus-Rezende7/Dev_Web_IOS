4b6053e2cd642e6364c22721eea345b5bdba761b
Mateus <mateusrezendedeolivera@gmail.comm>
2022-04-27 21:06:14 -0300

    Exercício js

---
+++ JS/02-Console, variáveis e operadores/Exercício.js
@@ -0,0 +1,13 @@
+console.log("Calculo IMC");
+
+
+var nome = "Mateus"
+var altura = 1.82
+var peso = 81
+
+var calculo = peso/(altura*altura);
+
+
+
+const texto = `${nome} possui uma altura de ${altura} e o peso de ${peso}, seu imd é ${calculo}`;
+console.log(texto);
