90ff4e8b896283caf5a3ef849394279161686ecd
Mateus DEV <mateusrezendedeoliveira@gmail.com>
2022-05-03 21:52:53 -0300

    js

---
+++ JS/04-Arrays/Exercício04.js
@@ -0,0 +1,31 @@
+var pessoas = new  Array('Joaquim', 'José', 'Silva' , 'Xavier')
+console.log(pessoas); 
+
+
+let z = pessoas.unshift('Amanda');   
+console.log(pessoas);
+
+pessoas.pop();
+console.log(pessoas)
+
+
+let y = pessoas.push('Zuleica')
+console.log(pessoas)
+
+delete pessoas[3]
+console.log(pessoas)
+
+pessoas [3]= "Silva";
+console.log(pessoas)
+
+pessoas.shift()
+console.log(pessoas)
+
+delete pessoas[2]
+console.log(pessoas)
+
+pessoas[2] = "silvana"
+console.log(pessoas)
+
+var pessoasString = pessoas.join('-')
+console.log(pessoasString)
