377b0c8da9d2f829f07afce1d091a5cf5278fcda
Mateus DEV <mateusrezendedeoliveira@gmail.com>
2022-05-11 21:50:36 -0300

    Aula 07 JS

---
+++ JS/07- estruturas condicionais/Exercício/main.js
@@ -0,0 +1,33 @@
+
+var colaboradores = {
+     cobarorador1:{
+         nome: 'Luva de pedreiro',
+         salario: 5690,
+         vinculo: 'PJ'
+
+     },
+    colaborador2:{
+        nome:'Samuel',
+        salario:1220,
+        vinculo:'PJ',
+    },
+    colaborador3:{
+        nome:'Joeferson',
+        salario:8123,
+        vinculo:'CLT',
+    },
+    colaborador4:{
+        nome:'Carlao',
+        salario:5432,
+        vinculo:'CLT',
+    },
+    colaborador5:{
+        nome:'Lebron James',
+        salario:4298,
+        vinculo:'CLT',
+      
+    },
+
+}
+
+
