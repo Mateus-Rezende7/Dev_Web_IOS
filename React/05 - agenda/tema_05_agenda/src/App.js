4331e912c9752c199582b0566ae5d042ef714641
MateusIOS <mateusrezendedeoliveira@gmail.com>
2022-06-06 21:49:58 -0300

    Aula 05 react

---
+++ React/05 - agenda/tema_05_agenda/src/App.js
@@ -0,0 +1,10 @@
+import Header from './Components/Header';
+function App() {
+  return (
+    <div className="container">
+      <Header  title="Compromissos"/>
+    </div>
+  );
+}
+
+export default App; 
